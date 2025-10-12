b7047250aed56024d5eb00d18fcef9b2ef223286
th3or14 <37025359+th3or14@users.noreply.github.com>
2025-10-12 07:20:56 +0300

    Add precompiler.py

---
+++ precompiler.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+
+if __name__ == '__main__':
+    usage = 'python3 precompiler.py -D A=1 -D B=2 -i in.glsl -o out.glsl'
+    parser = argparse.ArgumentParser(usage=usage)
+    parser.add_argument('-D', required=True, action='append')
+    parser.add_argument('-i', required=True)
+    parser.add_argument('-o', required=True)
+    args = parser.parse_args()
+    defines = ''
+    for d in args.D:
+        defines += ' '.join(['\n#define', *d.split('=', maxsplit=1)])
+    read_data = None
+    with open(args.i, encoding='utf-8') as input_file:
+        read_data = input_file.read()
+    version = re.search(r'#version .*', read_data)[0]
+    split_data = read_data.split(version, maxsplit=1)
+    final_data = split_data[0] + version + defines + split_data[1]
+    with open(args.o, 'w', encoding='utf-8') as output_file:
+        output_file.write(final_data)
