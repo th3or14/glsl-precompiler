e19f34182bd2f10a619b6f56737e58c8986bde4e
th3or14 <37025359+th3or14@users.noreply.github.com>
2025-10-15 04:30:45 +0300

    Rework testing

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,17 @@
 import filecmp
+import subprocess
 
 
-def test_input():
-    assert not filecmp.cmp('in.glsl', 'expected-out.glsl')
-
-
-def test_output():
+def test_usage():
+    args = ['python',
+            '../precompiler.py',
+            '-D',
+            'A=1',
+            '-D',
+            'B=2',
+            '-i',
+            'in.glsl',
+            '-o',
+            'out.glsl']
+    assert subprocess.run(args=args).returncode == 0
     assert filecmp.cmp('out.glsl', 'expected-out.glsl')
